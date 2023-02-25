2eb776344b9cf79971844e75423e29485bbb4169
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-02-26 01:16:23 +0900

    Fix: 型エラーを修正

--- scripts/start.ts
+++ scripts/start.ts
@@ -4,6 +4,6 @@ const rawDeployments = child_process.execSync("clasp deployments", {
   encoding: "utf-8",
 });
 
-const latestDeployment = rawDeployments.split("\n").at(-2).split(" ")[1];
+const latestDeployment = rawDeployments.split("\n").at(-2)!.split(" ")[1];
 
 child_process.execSync(`clasp deploy -i ${latestDeployment}`);
